0203d28e17a2a6403d4808128bc5e5a8cf311709
Ghislain <noupougi@gmail.com>
2018-11-05 15:03:14 -0500

    Add strategy doc

---
+++ z_blackfire_important_functions/set_environment_data.py
@@ -0,0 +1,14 @@
+description = """ This page shows all the step to follow to have all the data set download and ready to use.\n""" + \
+    """1. Download all stocks infos Data \n""" +\
+    """2. Download all Currency Data \n""" +\
+    """3. Download all stocks price Data \n""" +\
+    """4. Download all Price target Data \n""" +\
+    """5. Download all Consensus Data \n""" +\
+    """6. Patch Price target and consensus Data \n""" +\
+    """7. Merge price target and consensus to stocks data \n""" +\
+    """8. Download all naics infos \n""" +\
+    """9. Import zone eco and stock exchange \n""" +\
+    """10. Group stocks infos by sector \n""" +\
+    """11. Calculate return per sector \n"""
+
+print(description)
